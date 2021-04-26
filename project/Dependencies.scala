acf18b2e3183846225042d1ad8f9bdaf93efcb51
Scala Steward <me@scala-steward.org>
2021-04-26 23:33:22 +0200

    Update embedded-kafka to 2.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   lazy val testDependencies: Seq[ModuleID] = {
     Seq(scalaTest, scalacheck, discipline, embeddedKafka, logback) ++
       Seq(
-        "io.github.embeddedkafka" %% "embedded-kafka" % "2.3.0",
+        "io.github.embeddedkafka" %% "embedded-kafka" % "2.3.1",
         "org.apache.kafka" %% "kafka" % "2.3.0"
       )
   }.map(_ % Test)
