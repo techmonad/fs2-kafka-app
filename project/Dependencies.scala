fb1660ed9822f5051ea6d592fbb856bac220e9ed
Scala Steward <me@scala-steward.org>
2021-04-26 23:33:42 +0200

    Update embedded-kafka-schema-registry to 5.3.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   lazy val fs2KafkaVulcan = "com.ovoenergy" %% "fs2-kafka-vulcan" % "0.20.1"
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.8"
-  lazy val embeddedKafka = "io.github.embeddedkafka" %% "embedded-kafka-schema-registry" % "5.3.0"
+  lazy val embeddedKafka = "io.github.embeddedkafka" %% "embedded-kafka-schema-registry" % "5.3.2"
 
   lazy val scalacheck = "org.scalatestplus" %% "scalatestplus-scalacheck" % "3.1.0.0-RC2"
   lazy val discipline = "org.typelevel" %% "discipline-scalatest" % "1.0.0-M1"
