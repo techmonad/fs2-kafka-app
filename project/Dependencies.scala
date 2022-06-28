bda2ea3a315cb54e820ff45c45b7db57ee81a207
Anand Singh <imanandksingh@gmail.com>
2022-06-28 01:09:12 +0000

    Update kafka from 7.1.1-ce to 7.1.2-ce

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Dependencies {
     Seq(scalaTest, scalacheck, discipline, embeddedKafka, logback) ++
       Seq(
         "io.github.embeddedkafka" %% "embedded-kafka" % "3.1.0",
-        "org.apache.kafka" %% "kafka" % "7.1.1-ce"
+        "org.apache.kafka" %% "kafka" % "7.1.2-ce"
       )
   }.map(_ % Test)
 
