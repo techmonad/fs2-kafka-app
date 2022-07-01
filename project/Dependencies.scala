c4f036090991bb40842ef8251d29852a1e9240e9
Anand Singh <imanandksingh@gmail.com>
2022-07-01 01:14:01 +0000

    Update embedded-kafka-schema-registry from 7.1.0 to 7.1.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   lazy val fs2KafkaVulcan = "com.ovoenergy" %% "fs2-kafka-vulcan" % "0.20.2"
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.12"
-  lazy val embeddedKafka = "io.github.embeddedkafka" %% "embedded-kafka-schema-registry" % "7.1.0"
+  lazy val embeddedKafka = "io.github.embeddedkafka" %% "embedded-kafka-schema-registry" % "7.1.2"
 
   lazy val scalacheck = "org.scalatestplus" %% "scalatestplus-scalacheck" % "3.1.0.0-RC2"
   lazy val discipline = "org.typelevel" %% "discipline-scalatest" % "2.1.5"
