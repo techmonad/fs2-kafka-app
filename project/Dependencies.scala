cd508b53b3e7fb014b11463659f9de752ab3c23f
Anand Singh <4903911+anand-singh@users.noreply.github.com>
2022-10-11 01:09:53 +0000

    Update embedded-kafka from 3.2.3 to 3.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   lazy val testDependencies: Seq[ModuleID] = {
     Seq(scalaTest, scalacheck, discipline, embeddedKafka, logback) ++
       Seq(
-        "io.github.embeddedkafka" %% "embedded-kafka" % "3.2.3",
+        "io.github.embeddedkafka" %% "embedded-kafka" % "3.3.1",
         "org.apache.kafka" %% "kafka" % "7.2.2-ce"
       )
   }.map(_ % Test)
